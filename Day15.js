37f4ea65a18b878ee3e770fce71f59c9e5ade957
ishikabansal04 <49348251+ishikabansal04@users.noreply.github.com>
2020-10-02 20:53:37 +0530

    Adding code for Day15

---
+++ Day15.js
@@ -0,0 +1,10 @@
+var plant=document.getElementById("one");
+// var cls=
+// function grow(){
+//     document.getElementsById('myPlant').class='two';
+// }
+
+function grow(){
+    // plant.style.height="1000px";
+    document.getElementsById('myPlant').removeClass().addClass('two');
+}
